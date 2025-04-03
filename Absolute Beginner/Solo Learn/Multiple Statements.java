6a21340edc484b59baf6397d95a09993b11c933e
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-02 23:54:19 -0500

    Create Multiple Statements.java

---
+++ Absolute Beginner/Solo Learn/Multiple Statements.java
@@ -0,0 +1,9 @@
+public class Program
+{
+    public static void main(String[] args) {
+        System.out.println("Name:");
+        System.out.println("Surname:");
+	}
+}
+
+// Side note: Java is a case-sensitive language
