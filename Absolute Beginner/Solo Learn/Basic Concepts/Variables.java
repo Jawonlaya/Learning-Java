4daca7d7210762c84331e15b0ffc036e6531f33c
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-03 23:27:04 -0500

    Create Variables.java

---
+++ Absolute Beginner/Solo Learn/Basic Concepts/Variables.java
@@ -0,0 +1,17 @@
+class Demo {
+    public static void main(String[] args) {
+        String name;
+        name = "James";
+        System.out.println(name); 
+    }
+}
+
+// Combining declaration
+
+class Demo {
+    public static void main(String[] args) {
+        String name = "James";
+
+        System.out.println(name);
+    }
+}
