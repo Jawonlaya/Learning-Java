07593acce8df402e0f0c04c80b720e2ca4948631
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-03 23:57:01 -0500

    Create Doing Math.java

---
+++ Absolute Beginner/Solo Learn/Basic Concepts/Doing Math.java
@@ -0,0 +1,68 @@
+class Demo {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 3;
+
+        System.out.println(x+y);
+    }
+}
+
+
+class Demo {
+    public static void main(String[] args) {
+        double price1 = 24.99;
+        double price2 = 19.45;
+
+        double sum = price1 + price2;
+
+        System.out.println(sum);
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        int salary = 90000;
+        int tax = 15000;
+        int result = salary - tax;  
+
+        System.out.println(result);
+    }
+}
+
+// Note: the result of dividing two variable is a double
+class Demo {
+    public static void main(String[] args) {
+        int amount = 9000;
+        int price = 49;
+        int result = amount/price;
+
+        System.out.print(result);
+    }
+}
+
+
+class Demo {
+    public static void main(String[] args) {
+        int items = 23;
+        int res = items % 5; 
+
+        System.out.print(res);
+    }
+}
+
+/*
+Strings
+Java allows to add together strings, using the + operator. The result is the combination of the strings.
+For example, let's combine the first and last names to result in the full name of a user:
+*/
+class Demo {
+    public static void main(String[] args) {
+        String firstname = "James";
+        String lastname = "Smith";
+        String fullname = firstname+lastname;
+
+        System.out.print(fullname);
+    }
+}
+
+
