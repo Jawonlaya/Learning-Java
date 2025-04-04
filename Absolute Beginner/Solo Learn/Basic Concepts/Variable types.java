7a4af6aec7f5283afe48508707d4b0db5ab64864
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-03 23:36:43 -0500

    Create Variable types.java

---
+++ Absolute Beginner/Solo Learn/Basic Concepts/Variable types.java
@@ -0,0 +1,49 @@
+/*
+using an integer
+class Demo {
+    public static void main(String[] args) {
+        int age = 42; 
+
+        System.out.println(age);
+    }
+}
+*/
+
+// Using Decimals 
+/*
+class Demo {
+    public static void main(String[] args) {
+        double weight = 12.5; 
+
+        System.out.println(weight);
+    }
+}
+*/
+
+// Characters 
+
+/*
+
+class Demo {
+    public static void main(String[] args) {
+        char letter = 'B'; 
+
+        System.out.println(letter);
+    }
+}
+
+*/
+
+// Boolean 
+
+/*
+
+class Demo {
+    public static void main(String[] args) {
+        boolean isOpen = false; 
+
+        System.out.println(isOpen);
+    }
+}
+
+*/
