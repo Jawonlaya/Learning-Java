305b81cab4030c2f3c4a5170a37f4d4948b891df
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-04 00:11:03 -0500

    Create Bar_Inventory.java

---
+++ Absolute Beginner/Solo Learn/Basic Concepts/Bar_Inventory.java
@@ -0,0 +1,15 @@
+/*
+A bartender sold 64 bottles of juice and 23 bottles of ice tea. How many total bottles were sold?
+Task: Complete the code to calculate and output the total number of sold bottles.
+*/
+class Demo
+{
+    public static void main(String[] args) {
+        int juice = 64;
+        int iceTea= 23;
+        //calculate the sum and output it
+        int total = juice + iceTea;
+        System.out.println(total);
+            
+	}
+}
