38f4e63b34c5ace1d9c562ca6f0c2a349601e11f
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-03 23:31:32 -0500

    Create Storing a slogan excercise.java

---
+++ Absolute Beginner/Solo Learn/Basic Concepts/Storing a slogan excercise.java
@@ -0,0 +1,14 @@
+/*
+
+You want to show your friend how Java works in real time. In the given code you have the variable “slogan”.
+Task: Complete the code to output the slogan to the screen.
+*/
+class Demo {
+    public static void main(String[] args) {
+        String slogan = "Write once, run anywhere!";
+        System.out.println(slogan);
+        
+    }
+}
+
+
