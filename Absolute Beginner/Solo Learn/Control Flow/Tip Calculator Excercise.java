87e7c2107113f986a27e2cbe89496e9c377f60b3
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-06 23:09:17 -0500

    Update Tip Calculator Excercise.java

--- Absolute Beginner/Solo Learn/Control Flow/Tip Calculator Excercise.java
+++ Absolute Beginner/Solo Learn/Control Flow/Tip Calculator Excercise.java
@@ -7,3 +7,13 @@ Task: Take the bill amount as input and output the corresponding tip amount, whi
 
 To calculate 15% of a number, multiply it by 15, then divide by 100.
 */
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        //your code goes here
+        Scanner sc=new Scanner(System.in);
+        double bill=sc.nextDouble();
+        System.out.println(bill*0.15);
+    }
+}
