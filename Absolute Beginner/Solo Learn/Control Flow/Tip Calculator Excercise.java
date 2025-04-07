a3fee8791815d03727dcdd4b3ff67b492c978f49
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2025-04-06 23:05:33 -0500

    Create Tip Calculator Excercise.java

---
+++ Absolute Beginner/Solo Learn/Control Flow/Tip Calculator Excercise.java
@@ -0,0 +1,9 @@
+/*
+You always tip 15% of the bill amount. 
+
+To make the calculation easier, you decide to write a program that will do that for you.
+
+Task: Take the bill amount as input and output the corresponding tip amount, which should be 15% of the amount.
+
+To calculate 15% of a number, multiply it by 15, then divide by 100.
+*/
